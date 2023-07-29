2ab8baa486c46a03f8466965f9f7fbc3a149c6d0
lukashdias <lukas.dias@grupomateus.com.br>
2023-07-29 20:09:32 -0300

    add state

---
+++ src/routes/+count.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let count;
+	export let addCount;
+	export let minusCount;
+	export let clearCount;
+</script>
+
+<h2 class="text-white">Count: {count}</h2>
+<button
+	class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+	on:click={addCount}>Add</button
+>
+<button
+	class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+	on:click={minusCount}>Minus</button
+>
+<button
+	class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+	on:click={clearCount}>Clear</button
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let count = 0;
+
+	function addCount() {
+		count += 1;
+	}
+	function minusCount() {
+		count -= 1;
+	}
+	function clearCount() {
+		count = 0;
+	}
+
+	import Count from './+count.svelte';
+</script>
+
+<main class="w-screen h-screen bg-slate-900 flex justify-center text-center items-center">
+	<section class="flex flex-col gap-4">
+		<h1 class="text-white">Hello Svelte World</h1>
+		<Count {count} {addCount} {minusCount} {clearCount} />
+	</section>
+</main>
